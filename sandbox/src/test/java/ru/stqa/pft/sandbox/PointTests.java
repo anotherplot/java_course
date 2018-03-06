8cc8edf5ffe0b6445e3cd2ebb5aad8cfff9c445b
Olha Mezhova <mezhova.olha@gmail.com>
2018-03-06 21:12:17 +0300

    Задание 3

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,28 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.excersize2.Point;
+
+public class PointTests {
+
+
+  @Test
+  public void testArea(){
+    Point p1 = new Point(2,7);
+    Point p2 = new Point(-2,7);
+    Point p3 = new Point(-5,1);
+    Point p4 = new Point(-5,-7);
+    Point p5 = new Point(0,3);
+    Point p6 = new Point(-4,0);
+
+
+
+    Assert.assertEquals(p1.distance(p2),4.0);
+    Assert.assertEquals(p3.distance(p4),8.0);
+    Assert.assertEquals(p5.distance(p6),5.0);
+
+  }
+}
+
+
