99b06a636dca5d00b373a92e4b1d7c8fbacccdb0
Olha Mezhova <mezhova.olha@gmail.com>
2018-03-18 16:06:16 +0300

    Занятие по свертке вложенных проверок

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.sandbox;
 
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
 public class Equation {
 
   private double a;
@@ -8,23 +10,35 @@ public class Equation {
 
   private int n;
 
-  public Equation(double a, double b, double c){
+  public Equation(double a, double b, double c) {
 
     this.a = a;
     this.b = b;
     this.c = c;
 
-    double d = b*b - 4*a*c;
+    double d = b * b - 4 * a * c;
 
-    if (d > 0) {
-      n = 2;
-    } else {
-      if (d == 0){
-        n =1;
+    if (a != 0) {
+
+      if (d > 0) {
+        n = 2;
+      } else if (d == 0) {
+        n = 1;
       } else {
         n = 0;
       }
+
+
+    } else if (b != 0) {
+      n = 1;
+
+    } else if (c != 0) {
+      n = 0;
+
+    } else {
+      n = -1;
     }
+
 
   }
 
