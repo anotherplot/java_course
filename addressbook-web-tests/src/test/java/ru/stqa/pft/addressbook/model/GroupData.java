a6197ab589ae9c892d6d45be9cbf68ef87cf76ba
Olha Mezhova <mezhova.olha@gmail.com>
2018-03-27 22:49:54 +0300

    Доработка теста по модификации группы с неупорядоченным множеством

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -6,8 +6,36 @@ public class GroupData {
   private final String name;
   private final String header;
   private final String footer;
+  private final String id;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(name, groupData.name) &&
+            Objects.equals(id, groupData.id);
+  }
+
+  @Override
+  public int hashCode() {
+
+    return Objects.hash(name, id);
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public GroupData(String id, String name, String header, String footer) {
+    this.id = id;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
 
   public GroupData(String name, String header, String footer) {
+    this.id = null;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -21,21 +49,8 @@ public class GroupData {
   public String toString() {
     return "GroupData{" +
             "name='" + name + '\'' +
+            ", id='" + id + '\'' +
             '}';
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-
-    return Objects.hash(name);
   }
 
   public String getHeader() {
