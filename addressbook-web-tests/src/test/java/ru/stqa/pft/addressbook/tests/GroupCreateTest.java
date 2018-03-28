81c61230e0bc7804dafd625f8e19cd0f1e9389d1
Olha Mezhova <mezhova.olha@gmail.com>
2018-03-28 22:51:09 +0300

    Вычисление максимального идектификатора с лямбда-выражением

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreateTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreateTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -19,14 +20,8 @@ public class GroupCreateTest extends TestBase {
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
     Assert.assertEquals(new HashSet<Object>(before),  new HashSet<Object>(after));
 
 
