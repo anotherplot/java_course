6bae32beb8c484e74dba3873c6c31314339d4b82
Olha Mezhova <mezhova.olha@gmail.com>
2018-04-21 20:29:31 +0300

    Сравнение данных в бд при удалении и создании групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreateTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreateTest.java
@@ -60,10 +60,10 @@ public class GroupCreateTest extends TestBase {
   public void testGroupCreation(GroupData group) {
 
     app.goTo().groupPage();
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size()+1));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
@@ -72,12 +72,12 @@ public class GroupCreateTest extends TestBase {
   public void testBadGroupCreation() {
 
     app.goTo().groupPage();
-    Groups before = app.group().all();
-
+    Groups before = app.db().groups();
     GroupData group = new GroupData().withName("test'");
+    app.goTo().groupPage();
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size()));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after, equalTo(before));
   }
 
