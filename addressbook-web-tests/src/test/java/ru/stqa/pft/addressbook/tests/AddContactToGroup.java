9de15e9c75a241886d8f9e5f3a42eb3f00cd71b8
Olha Mezhova <mezhova.olha@gmail.com>
2018-04-23 00:49:47 +0300

    Добавлены проверки послп добавления контакта в группу

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactToGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactToGroup.java
@@ -9,6 +9,10 @@ import ru.stqa.pft.addressbook.model.Groups;
 
 import java.io.File;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertTrue;
+
 public class AddContactToGroup extends TestBase{
 
 
@@ -38,14 +42,14 @@ public class AddContactToGroup extends TestBase{
     public void testAddContactToGroup() {
 
         ContactData choosenContact = app.db().contacts().iterator().next();
-        Groups contactGroups = choosenContact.getGroups();
+        Groups contactGroupsBefore = choosenContact.getGroups();
 
         Groups groups = app.db().groups();
         GroupData chosenGroup = groups.iterator().next();
 
-        if (contactGroups.size() != 0 && contactGroups.iterator().next().getId() == chosenGroup.getId())
+        if (contactGroupsBefore.size() != 0 && contactGroupsBefore.iterator().next().getId() == chosenGroup.getId())
             for (GroupData existingGroup : groups) {
-                    if (! contactGroups.contains(existingGroup)) {
+                    if (! contactGroupsBefore.contains(existingGroup)) {
                         chosenGroup = existingGroup;
                         System.out.println("found another free group"+existingGroup);
                         break;
@@ -59,6 +63,9 @@ public class AddContactToGroup extends TestBase{
         app.contact().selectContactById(choosenContact.getId());
         choosenContact.inGroup(chosenGroup);
         app.contact().addGroupTo(choosenContact, chosenGroup);
+        Groups contactGroupsAfter = choosenContact.getGroups();
+        assertThat(contactGroupsBefore.size()+1, equalTo(contactGroupsAfter.size()));
+        assertTrue(contactGroupsAfter.contains(chosenGroup));
 
     }
 
